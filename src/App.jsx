a8e324a2a84b9919b7b38a63b32a886b15086c21
mbarto202 <michael.d.barto@gmail.com>
2024-05-08 12:19:57 -0400

    organizing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './header/Header'
+
+const App = () => {
+  return (
+    <div className="App">
+      boo
+    </div>
+  );
+}
+
+export default App;
