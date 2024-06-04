66311fd704999ea20a2b3e5e57e5e484ff84a7e1
mbarto202 <michael.d.barto@gmail.com>
2024-06-04 09:42:59 -0400

    started header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,7 +3,16 @@ import App from '../App'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <div className='titleBg'> 
+          <h1 className='name'>Michael Barto</h1>
+          <h5 className="title">..</h5>
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -4,15 +4,20 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {BiMessageDetail} from 'react-icons/bi'
+import { RiHomeLine } from "react-icons/ri";
+import { BiMessage } from "react-icons/bi";
+import { RiHome7Line } from "react-icons/ri";
+import { BiMessageDots } from "react-icons/bi";
+import { TbFlame } from "react-icons/tb";
 import {useState} from 'react'
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-    <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active': ''}><AiOutlineHome/></a>
-    <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
-    <a href="#contact" onClick={() => setActiveNav('#footer')} className={activeNav === '#footer' ? 'active' : ''}><BiMessageDetail/></a>
+    <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active': ''}><RiHome7Line /></a>
+    <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><TbFlame /></a>
+    <a href="#contact" onClick={() => setActiveNav('#footer')} className={activeNav === '#footer' ? 'active' : ''}><BiMessageDots /></a>
   </nav>
   )
 }
