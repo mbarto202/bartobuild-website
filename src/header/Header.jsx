ee34ad142ea7ec3beb9963067fd565d32f5c053e
mbarto202 <michael.d.barto@gmail.com>
2024-06-13 11:14:28 -0400

    header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './header.css'
 import App from '../App'
 
 export const Header = () => {
@@ -7,7 +8,7 @@ export const Header = () => {
       <div className="container header_container">
         <div className='titleBg'> 
           <h1 className='name'>Michael Barto</h1>
-          <h5 className="title">..</h5>
+          <h5 className="title">Title</h5>
         </div>
 
         <a href="#contact" className='scroll_down'>Scroll Down</a>
