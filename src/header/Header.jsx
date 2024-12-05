b511bbc61d3cfb59036edbb6af82a1728c285820
mbarto202 <michael.d.barto@gmail.com>
2024-12-04 23:25:23 -0500

    header modernized

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import './header.css'
-import HeaderSocial from './HeaderSocials'
-import App from '../App'
+import React from "react";
+import "./header.css";
+import HeaderSocial from "./HeaderSocials";
 
-export const Header = () => {
+const Header = () => {
   return (
     <header>
       <div className="container header_container">
-        <div className='titleBg'> 
-          <h1 className='name'>Michael Barto</h1>
-          <h5 className="title">Fitness Coach</h5>
-          <a className='btn btn-primary'>Book a call</a>
+        <div className="titleBg">
+          <h1 className="name">Michael Barto</h1>
+          <h5 className="title">Your Personal Fitness Coach</h5>
+          <a href="#contact" className="btn btn-primary">
+            Book a Call
+          </a>
         </div>
         <HeaderSocial />
-
-        <a href="#contact" className='scroll_down'>Scroll Down</a>
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
