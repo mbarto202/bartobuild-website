0c000e7179749da7b007ba06dd73656450a74db6
mbarto202 <michael.d.barto@gmail.com>
2024-12-06 17:40:56 -0500

    about section created

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./about.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section id="about" className="about-section">
+      <h2 className="section-title">About Me</h2>
+      <p className="about-text">
+        Welcome to BartoBuild! I’m Michael Barto, a fitness coach passionate
+        about helping you achieve your dream physique. With years of experience
+        in bodybuilding and personal training, I specialize in personalized
+        fitness plans tailored to your goals.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
