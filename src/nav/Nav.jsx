fb1951ba321e48259271886dc79700f3a42e82b2
Michael <michael.d.barto@gmail.com>
2025-02-14 18:14:00 -0500

    made services button on nav

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {BiBook} from 'react-icons/bi'
-import {BiMessageDetail} from 'react-icons/bi'
-import { RiHomeLine } from "react-icons/ri";
-import { BiMessage } from "react-icons/bi";
+import React from "react";
+import "./nav.css";
 import { RiHome7Line } from "react-icons/ri";
 import { BiMessageDots } from "react-icons/bi";
 import { TbFlame } from "react-icons/tb";
-import {useState} from 'react'
+import { useState } from "react";
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#')
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-    <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active': ''}><RiHome7Line /></a>
-    <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><TbFlame /></a>
-    <a href="#contact" onClick={() => setActiveNav('#footer')} className={activeNav === '#footer' ? 'active' : ''}><BiMessageDots /></a>
-  </nav>
-  )
-}
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <RiHome7Line />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <TbFlame />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#footer")}
+        className={activeNav === "#footer" ? "active" : ""}
+      >
+        <BiMessageDots />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
