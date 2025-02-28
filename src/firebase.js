25cf9c1159012613e4dd31eb62a3cc50fc728790
Michael <michael.d.barto@gmail.com>
2025-02-27 22:46:13 -0500

    firebase adjusted for faceid

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,9 @@ import {
   getAuth,
   GoogleAuthProvider,
   signInWithCredential,
+  EmailAuthProvider,
 } from "firebase/auth";
+import { register, authenticate } from "passkeys-js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDQ1rdWhPN7GQtjzmU-n-nVv8SWPdVPp1o",
@@ -19,4 +21,35 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, provider, signInWithCredential };
+const registerWithPasskey = async () => {
+  try {
+    const credential = await register();
+    return credential; // Save this credential for authentication
+  } catch (error) {
+    console.error("Passkey Registration Error:", error);
+    return null;
+  }
+};
+
+//Authenticate user with Passkeys (Face ID)
+const authenticateWithPasskey = async () => {
+  try {
+    const credential = await authenticate();
+    const firebaseCredential = EmailAuthProvider.credential(
+      credential.id,
+      credential.rawId
+    );
+    return signInWithCredential(auth, firebaseCredential);
+  } catch (error) {
+    console.error("Passkey Authentication Error:", error);
+    return null;
+  }
+};
+
+export {
+  auth,
+  provider,
+  signInWithCredential,
+  registerWithPasskey,
+  authenticateWithPasskey,
+};
