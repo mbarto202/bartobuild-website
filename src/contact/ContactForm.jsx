d42a013d0acdaa03962666195e0489c8729f11d8
Michael <michael.d.barto@gmail.com>
2025-03-06 21:26:56 -0500

    styled form

--- src/contact/ContactForm.jsx
+++ src/contact/ContactForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { auth } from "../firebase";
+import "./ContactForm.css";
 
 const ContactForm = () => {
   const user = auth.currentUser; // Get logged-in user
