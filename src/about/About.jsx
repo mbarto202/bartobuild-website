ceb2f843f08d78f1f6f111548f305b207de78c06
Michael <michael.d.barto@gmail.com>
2025-05-11 16:47:42 -0400

    final touches before hosting

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./about.css";
+import sidechest from "../assets/sidechest.jpg";
 
 const About = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -19,16 +20,14 @@ const About = () => {
   return (
     <section id="about" className={`about-section ${isVisible ? "show" : ""}`}>
       <div className="about-content">
-        <img
-          src="/path-to-your-image.jpg"
-          alt="Michael Barto"
-          className="about-image"
-        />
+        <img src={sidechest} alt="Michael Barto" className="about-image" />
         <div className="about-text">
           <h2 className="section-title">About Me</h2>
           <p>
-            Welcome! I’m Michael Barto, a fitness coach passionate about helping
-            you achieve your dream physique.
+            Welcome! I’m Michael, a fitness coach passionate about helping you
+            achieve your dream physique. I’ve competed in natural bodybuilding
+            for two years and have experience coaching clients through personal
+            training and contest prep.
           </p>
           <a href="#services" className="about-btn">
             Learn More <span className="arrow">→</span>
