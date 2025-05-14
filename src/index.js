eacee4ed75014922f20f55b66700115a2b2539a1
Michael <michael.d.barto@gmail.com>
2025-05-14 11:17:53 -0400

    removed auth0 and cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -4,20 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Auth0Provider
-    domain="dev-z4w2x45wvwgu7k23.us.auth0.com"
-    clientId="5lxjbRkWTXOQYvhOIv1nrTliAXvvuZyg"
-    authorizationParams={{ redirect_uri: window.location.origin }}
-  >
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Auth0Provider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 reportWebVitals();
