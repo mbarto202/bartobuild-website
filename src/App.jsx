7b60ed57e6061e43d1a95583705f6e4c73b95ba3
Michael <michael.d.barto@gmail.com>
2026-06-29 21:20:22 -0400

    refactor: temporarily remove contact form section

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
               <>
                 <About />
                 <Services />
-                <ContactForm />
+                {/*<ContactForm />*/}
               </>
             }
           />
